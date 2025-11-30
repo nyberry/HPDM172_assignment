b6511ddf6313f7b44bca281d96148a6dc6d45a4c
qaadiro <abdiqdr93@gmail>
2025-11-30 14:18:36 +0000

    Add sample_data.sql with 40 hospitals

---
+++ mysql/sample_data.sql
@@ -0,0 +1,51 @@
+INSERT INTO hospital
+    (name, address, beds, accreditation_date,
+     has_emergency_department, is_teaching_hospital)
+VALUES
+    ('St Thomas Hospital', 'Westminster Bridge Road, London, England', 840, '2005-03-15', 1, 1),
+    ('University College Hospital', 'Euston Road, London, England', 720, '2007-06-20', 1, 1),
+    ('Royal London Hospital', 'Whitechapel Road, London, England', 900, '2003-11-05', 1, 1),
+    ('Kings College Hospital', 'Denmark Hill, London, England', 780, '2006-09-12', 1, 1),
+    ('Guy''s Hospital', 'Great Maze Pond, London, England', 650, '2004-02-28', 1, 1),
+
+    ('Birmingham General Hospital', '1 Broad Street, Birmingham, England', 600, '2010-01-10', 1, 0),
+    ('Queen Elizabeth Hospital Birmingham', 'Mindelsohn Way, Birmingham, England', 1000, '2008-05-19', 1, 1),
+    ('Solihull District Hospital', 'Lode Lane, Solihull, England', 320, '2012-07-03', 1, 0),
+    ('Wolverhampton City Hospital', 'New Cross Road, Wolverhampton, England', 550, '2011-09-22', 1, 0),
+    ('Coventry Central Hospital', 'Foleshill Road, Coventry, England', 430, '2013-04-17', 1, 0),
+
+    ('Manchester Royal Infirmary', 'Oxford Road, Manchester, England', 800, '2009-02-14', 1, 1),
+    ('Salford Community Hospital', 'Langworthy Road, Salford, England', 260, '2014-06-01', 0, 0),
+    ('Trafford General Hospital', 'Moorside Road, Manchester, England', 380, '2010-10-10', 1, 0),
+    ('Stockport District Hospital', 'Buxton Road, Stockport, England', 420, '2011-03-08', 1, 0),
+    ('Bolton Royal Hospital', 'Chorley New Road, Bolton, England', 390, '2012-11-25', 1, 0),
+
+    ('Leeds General Infirmary', 'Great George Street, Leeds, England', 750, '2007-08-18', 1, 1),
+    ('St James University Hospital', 'Beckett Street, Leeds, England', 710, '2006-12-02', 1, 1),
+    ('Bradford Royal Infirmary', 'Duckworth Lane, Bradford, England', 540, '2009-05-23', 1, 0),
+    ('Huddersfield District Hospital', 'Acre Street, Huddersfield, England', 360, '2013-01-30', 1, 0),
+    ('Wakefield Community Hospital', 'Northgate, Wakefield, England', 280, '2014-09-09', 0, 0),
+
+    ('Newcastle General Hospital', 'Westgate Road, Newcastle upon Tyne, England', 580, '2008-04-11', 1, 0),
+    ('Royal Victoria Infirmary', 'Queen Victoria Road, Newcastle upon Tyne, England', 620, '2005-01-26', 1, 1),
+    ('Sunderland City Hospital', 'Chester Road, Sunderland, England', 450, '2011-07-14', 1, 0),
+    ('Middlesbrough Riverside Hospital', 'Riverside Park, Middlesbrough, England', 390, '2012-05-07', 1, 0),
+    ('Durham County Hospital', 'Framwellgate Peth, Durham, England', 340, '2013-10-19', 0, 0),
+
+    ('Bristol Royal Infirmary', 'Upper Maudlin Street, Bristol, England', 670, '2006-06-06', 1, 1),
+    ('Southmead Hospital', 'Southmead Road, Bristol, England', 720, '2010-02-02', 1, 1),
+    ('Bath City Hospital', 'London Road, Bath, England', 310, '2011-11-01', 1, 0),
+    ('Gloucester County Hospital', 'Great Western Road, Gloucester, England', 380, '2012-08-29', 1, 0),
+    ('Swindon General Hospital', 'Marlborough Road, Swindon, England', 360, '2014-03-21', 1, 0),
+
+    ('Nottingham City Hospital', 'Hucknall Road, Nottingham, England', 640, '2007-05-13', 1, 1),
+    ('Queen''s Medical Centre Nottingham', 'Derby Road, Nottingham, England', 820, '2009-09-09', 1, 1),
+    ('Derby Royal Hospital', 'Uttoxeter Road, Derby, England', 500, '2011-12-15', 1, 0),
+    ('Leicester Royal Infirmary', 'Infirmary Square, Leicester, England', 730, '2008-03-27', 1, 1),
+    ('Lincoln County Hospital', 'Greetwell Road, Lincoln, England', 410, '2013-07-12', 1, 0),
+
+    ('Sheffield General Hospital', 'Glossop Road, Sheffield, England', 690, '2006-04-04', 1, 1),
+    ('Royal Hallamshire Hospital', 'Glossop Road, Sheffield, England', 650, '2005-10-18', 1, 1),
+    ('Doncaster District Hospital', 'Thorne Road, Doncaster, England', 370, '2012-01-06', 1, 0),
+    ('Rotherham Community Hospital', 'Moorgate Road, Rotherham, England', 290, '2014-11-24', 0, 0),
+    ('Chesterfield Royal Hospital', 'Calow, Chesterfield, England', 430, '2010-09-03', 1, 0);
