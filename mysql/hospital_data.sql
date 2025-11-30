bbfd93b72c5345567ed9a2cfb8957f1a316913aa
qaadiro <abdiqdr93@gmail>
2025-11-30 21:45:09 +0000

    Add hospital_data.sql with hospital insert statements

---
+++ mysql/hospital_data.sql
@@ -0,0 +1,9 @@
+/* ============================================
+   MASTER SAMPLE DATA LOADER
+   Runs all individual data scripts such as:
+     - hospital_data.sql
+     - doctor_data.sql
+     - patient_data.sql
+   ============================================ */
+
+
